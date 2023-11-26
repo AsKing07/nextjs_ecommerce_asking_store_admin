7234c3cb4b7602d5cadad09964d0d9dbe6314c37
Charbel Sonon <charbelsnn@gmail.com>
2023-11-26 02:19:11 +0100

    design Nav component and principale layout

---
+++ components/Layout.js
@@ -0,0 +1,36 @@
+import { Inter } from 'next/font/google'
+import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from '@/components/Nav';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function  Layout({children}){
+  const {data: session} = useSession();
+  if(!session)
+  {
+    return (
+      <div className='bg-blue-900 w-screen h-screen flex items-center'>
+        <div className='text-center w-full'>
+          <button 
+            onClick={() =>signIn('google')}
+            className='bg-white p-2 px-4 rounded-lg'>
+            Se connecter avec Google
+          </button>
+        </div>
+      </div>
+      )
+  }
+  else
+  {
+    return(
+      <div className='bg-blue-900 min-h-screen flex'>
+                <Nav />
+                <div className='bg-white flex-grow mt-1 mr-1 mb-2 rounded-lg p-4'>{children}</div>
+      </div>
+    )
+  }
+
+}
+
+
+///pages/api/auth/[...nextauth].js
--- pages/index.js
+++ pages/index.js
@@ -2,34 +2,12 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useSession, signIn, signOut } from "next-auth/react"
 import Nav from '@/components/Nav';
+import Layout from '@/components/Layout';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const {data: session} = useSession();
-  if(!session)
-  {
-    return (
-      <div className='bg-blue-900 w-screen h-screen flex items-center'>
-        <div className='text-center w-full'>
-          <button 
-            onClick={() =>signIn('google')}
-            className='bg-white p-2 px-4 rounded-lg'>
-            Se connecter avec Google
-          </button>
-        </div>
-      </div>
-      )
-  }
-  else
-  {
-    return(
-      <div className='bg-blue-900 min-h-screen'>
-                <Nav />
-                <div>Connecté en tant que {session.user.email}</div>
-      </div>
-    )
-  }
+  return <Layout>test</Layout>
 
 }
 
